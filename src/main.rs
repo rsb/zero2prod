17ee9e9ac19a62ca8390c655a4d2feacf05e6ffe
Robert Scott-Buccleuch <60840+rsb@users.noreply.github.com>
2023-04-18 15:35:31 -0700

    3.5.1 - Polish

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
-
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-  run()?.await
+async fn main() -> std::io::Result<()> {
+  let listener = TcpListener::bind("127.0.0.1:8000")?;
+  run(listener)?.await
 }
